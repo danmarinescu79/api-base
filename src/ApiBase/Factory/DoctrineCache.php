e91e4da877e9e34a0754f0a77b129d5671d82ade
Dan Marinescu <dan.marinescu79@icloud.com>
2019-10-02 12:57:00 +0300

    static call memcached

--- src/ApiBase/Factory/DoctrineCache.php
+++ src/ApiBase/Factory/DoctrineCache.php
@@ -6,10 +6,17 @@ class DoctrineCache
 {
     public function __construct()
     {
-        $cache    = new \Doctrine\Common\Cache\MemcachedCache();
+        // $cache    = new \Doctrine\Common\Cache\MemcachedCache();
+        // $memcache = new \Memcached();
+        // $memcache->addServer('localhost', 11211);
+        // $cache->setMemcached($memcache);
+        // return $memcache;
+    }
+
+    public static function getMemcached()
+    {
         $memcache = new \Memcached();
         $memcache->addServer('localhost', 11211);
-        $cache->setMemcached($memcache);
         return $memcache;
     }
 }
